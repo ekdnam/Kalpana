24f74609d09e23b6320d0aa01bfa9d7a8a3ebdb8
ekdnam <adeetya.m@gmail.com>
2020-10-31 23:08:49 +0530

    try out

--- flycatcher/__init__.py
+++ flycatcher/__init__.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-from .models import cGAN
-from .models import acgan
-=======
 from flycatcher import models
 
 __version__ = "v0.0.1a"
 
 name = "flycatcher"
->>>>>>> 453be19844f45ea0a3f81fe5b3c389328e2eab86
